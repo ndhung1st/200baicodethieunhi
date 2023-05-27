8871684c5c90c79b5a3ed5098f65be228e7df160
Hung Nguyen <121171582+ndhung1st@users.noreply.github.com>
2023-05-26 20:36:34 -0700

    Delete chu_de_toan_hoc directory

--- chu_de_toan_hoc/bai2.py
+++
@@ -1,5 +0,0 @@
-arr = []
-for i in range(int(input())):
-    arr.append(7 - int(input()))
-for i in arr:
-    print(i)
--- chu_de_toan_hoc/bai5.py
+++
@@ -1,6 +0,0 @@
-arr = []
-for i in range(int(input())):
-    n = int(input())
-    arr.append(n*(n - 1))           # mỗi người cần gửi (N - 1) tin
-for i in arr:                       # N người cần gửi N*(N - 1) tin
-    print(i)
